daa8d935b93e45c5e6ec7780b1ce6d81e6656ce2
Elia Schito <elia@schito.me>
2019-02-04 23:17:36 +0100

    Use a more comprehensive example in /try

--- source/javascripts/try_opal.rb
+++ source/javascripts/try_opal.rb
@@ -1,23 +1,68 @@
 require 'opal'
 require 'opal-parser'
 require 'opal-jquery'
+require 'js'
 
-DEFAULT_TRY_CODE = <<-RUBY
-class User
-  attr_accessor :name
+DEFAULT_TRY_CODE = <<-'RUBY'
+say = "I love Ruby"
+puts say
 
-  def initialize(name)
-    @name = name
-  end
+puts say.sub('love', "*love*").upcase
 
-  def admin?
-    @name == 'Admin'
-  end
+5.times { puts say }
+
+
+def title(s)
+  puts "\n\n~ #{s} ~\n" + '~' * (s.size+4) + "\n\n"
+  yield
 end
 
-user = User.new('Bob')
-puts user
-puts user.admin?
+title "Interacting with the DOM" do
+  require 'native'
+
+  puts "The page title is #{$$[:document][:title].inspect}."
+  puts "You're viewing #{$$[:location][:href]}."
+
+  # Uncomment the following lines to ask for a name:
+  # name = $$.prompt "Please enter your name"
+  # $$.alert("Hello #{name}!")
+end
+
+title "Classes, objects and procs" do
+  class User
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def admin?
+      @name == 'Joe'
+    end
+
+    def method_missing(name, *args, &block)
+      if name.start_with?('can_') && name.end_with?('?')
+        admin? ? true : false
+      end
+    end
+  end
+
+  bob = User.new('Bob')
+  joe = User.new('Joe')
+
+  user_is_admin = -> user do
+    "#{user.name} #{user.admin? ? 'is' : 'is not'} an admin."
+  end
+
+  user_can_swim = -> user, action do
+    "#{user.name} #{user.can_swim? ? 'can' : 'cannot'} swim."
+  end
+
+  puts user_is_admin.call joe
+  puts user_can_swim.call joe
+  puts user_is_admin.call bob
+  puts user_can_swim.call bob
+end
 RUBY
 
 class TryOpal
