c6ec335fa9fe1afd63d249a55b74a816f0cd4db0
Elia Schito <elia@schito.me>
2021-06-28 11:04:28 +0200

    Ditching bootstrap.js

--- assets/config/manifest.js
+++ assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link application.debug.css
 //= link load-parser.debug.js
 //= link application.debug.js
-//= link bootstrap.debug.js
 //= link try.debug.js
--- assets/javascripts/application.js.rb
+++ assets/javascripts/application.js.rb
@@ -2,3 +2,10 @@ require 'opal'
 require 'native'
 require 'console'
 require 'js'
+
+$$[:document].addEventListener :DOMContentLoaded do
+  $$[:document].querySelector('.navbar-toggle').addEventListener :click do |event|
+    target = $$[:document].querySelector event.JS[:currentTarget].JS[:dataset].JS[:target]
+    target[:classList].toggle(event.JS[:currentTarget].JS[:dataset].JS[:toggle])
+  end
+end
