732822865e07c2a970c56772112661b56d9a70e1
hmdne <54514036+hmdne@users.noreply.github.com>
2023-11-09 12:02:56 +0100

    Attempt to restore GitHub pages

--- assets/javascripts/_playground/editor.rb
+++ assets/javascripts/_playground/editor.rb
@@ -1,3 +1,5 @@
+# backtick_javascript: true
+
 module Playground
   class Editor
     OPTIONS = { lineNumbers: true, theme: 'tomorrow-night-eighties' }
--- assets/javascripts/try.js.rb
+++ assets/javascripts/try.js.rb
@@ -1,3 +1,5 @@
+# backtick_javascript: true
+
 require '_vendor/codemirror'
 require '_vendor/ruby'
 require '_vendor/javascript'
